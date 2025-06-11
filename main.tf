91da805a1e08f231456d31f1e4f8b491e82afc09
403DevOps <nzaitcev94@gmail.com>
2025-06-11 19:21:20 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-task1-rs"
+    bucket = "terraform-state-task1-rs1"
     key    = "terraform/state.tfstate"
     region = "eu-central-1"
     dynamodb_table = "terraform-state-lock"
